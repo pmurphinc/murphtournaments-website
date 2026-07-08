d4bc10bc093dfccf4035fcf9a5fd9074952b91b4
pmurphinc <pmurphinc@gmail.com>
2026-07-08 15:09:13 -0700

    Add statement breakpoints to tournament owner map migration

--- drizzle/0019_tournament_maps_and_owners.sql
+++ drizzle/0019_tournament_maps_and_owners.sql
@@ -1,4 +1,7 @@
 ALTER TABLE `tournaments` ADD `ownerUserId` int;
+--> statement-breakpoint
 ALTER TABLE `tournament_games` ADD `mapId` varchar(64);
+--> statement-breakpoint
 ALTER TABLE `tournament_control_template_games` ADD `mapId` varchar(64);
+--> statement-breakpoint
 ALTER TABLE `tournaments` ADD CONSTRAINT `tournaments_ownerUserId_users_id_fk` FOREIGN KEY (`ownerUserId`) REFERENCES `users`(`id`) ON DELETE set null ON UPDATE no action;
