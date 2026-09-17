c9341c1981b1ff0562e4476385405455703155bb
pmurphinc <pmurphinc@gmail.com>
2026-09-16 17:59:37 -0700

    Add Season 11 Respec Order update

---
+++ client/src/data/respecOrder3.ts
@@ -0,0 +1,86 @@
+export const RESPEC_ORDER_3_VERSION = "11.10.0";
+export const RESPEC_ORDER_3_PATCH_URL =
+  "/patchnotes?version=11.10.0#patch-11-10-0";
+export const RESPEC_ORDER_3_OFFICIAL_URL =
+  "https://www.reachthefinals.com/patchnotes/s11-respec-order-3";
+
+export const RESPEC_ORDER_3_WEAPONS = [
+  {
+    name: "Dagger",
+    slug: "dagger",
+    class: "Light",
+    summary: "Backstab retuned",
+  },
+  {
+    name: "LH1",
+    slug: "lh1",
+    class: "Light",
+    summary: "Shorter damage range",
+  },
+  {
+    name: "Sword",
+    slug: "sword",
+    class: "Light",
+    summary: "Damage reduced",
+  },
+  {
+    name: "CB-01 Repeater",
+    slug: "cb-01-repeater",
+    class: "Medium",
+    summary: "Damage increased",
+  },
+  {
+    name: "Chimera-XB",
+    slug: "chimera-xb",
+    class: "Medium",
+    summary: "Damage increased",
+  },
+  {
+    name: "Dual Blades",
+    slug: "dual-blades",
+    class: "Medium",
+    summary: "Damage increased",
+  },
+  {
+    name: "P90",
+    slug: "p90",
+    class: "Medium",
+    summary: "Damage increased",
+  },
+  {
+    name: "Pike-556",
+    slug: "pike-556",
+    class: "Medium",
+    summary: "Damage increased",
+  },
+  {
+    name: "Riot Shield",
+    slug: "riot-shield",
+    class: "Medium",
+    summary: "Damage increased",
+  },
+  {
+    name: "BFR Titan",
+    slug: "bfm-titan",
+    class: "Heavy",
+    summary: "Damage and reload buffed",
+  },
+  {
+    name: "Lewis Gun",
+    slug: "lewis-gun",
+    class: "Heavy",
+    summary: "Fire rate increased",
+  },
+  {
+    name: "M60",
+    slug: "m60",
+    class: "Heavy",
+    summary: "Fire rate increased",
+  },
+  {
+    name: "MGL-32",
+    slug: "mgl32",
+    class: "Heavy",
+    summary: "Damage increased",
+  },
+] as const;
